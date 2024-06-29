f81b91a592695907a0b3528b60fae574a8b6a949
Techie-krishna-kayaking <krishnakyk@gmail.com>
2024-06-29 12:25:30 +0530

    tkk

---
+++ 1-basics.py
@@ -0,0 +1,78 @@
+"""
+# Author - Techie Krishna Kayaking
+# Python Practice - 29 Jun 2024
+
+"""
+
+import os
+
+print("Hello Techie Krishna Kayaking")
+
+# Practice Printing
+print("""
+Hare Krishna Hare Krishna Krishna Krishna Hare Hare
+Hare Rama Hare Rama Rama Rama Hare Hare 
+""")
+
+# List files in directory
+print(os.listdir("C:\\Users\\Administrator\\PycharmProjects"))
+
+# Variables
+a = 'TKK'
+b = 7
+c = 29.9
+
+print(a)
+print(b)
+print(c)
+
+# typeof variables
+print(type(a))
+print(type(b))
+
+# OPERATORS
+a = 10
+b = 5
+
+print("Answer", a, "+", b, "=",  a+b)
+print("Answer", a, "+", b, "=",  a-b)
+print("Answer", a, "+", b, "=",  a*b)
+print("Answer", a, "+", b, "=",  a/b)
+print("Answer", a, "+", b, "=",  a%b)
+
+
+# ASSIGNMENT OPERATORS [ =, +=, -=, *=, /= ]
+a = 100
+print(a)
+a += 10
+print(a)
+a -= 50
+print(a)
+a *= 10
+print(a)
+a /= 5
+print(a)
+
+
+# COMPARISON OPERATOR [>, <, >=, <=, ==, !=]
+
+
+# LOGICAL OPERATOR [ OR, AND, NOT ]
+
+
+
+# TYPE CASTING - [ str(), int(), float() ]
+a = "77"
+a = int(a)
+print(type(a))
+print(a + 3)
+
+
+
+
+
+
+
+
+
+
